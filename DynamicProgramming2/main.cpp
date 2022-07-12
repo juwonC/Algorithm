229ca9e28c142c409a7c016b9446457677081cdc
JuwonCha <juwon1512@gmail.com>
2022-07-12 23:45:49 +0900

    Add CanAccumulate history

--- DynamicProgramming2/main.cpp
+++ DynamicProgramming2/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
-bool CanAccumulate(int sum, const std::vector<int>& numbers)
+using history = std::map<int, bool>;
+
+bool CanAccumulate(int sum, const std::vector<int>& numbers, history& h)
 {
+	// solved
+	if (h.count(sum) == 1)
+	{
+		return h[sum];
+	}
+	
 	// base case
 	if (sum == 0)
 	{
@@ -19,19 +28,30 @@ bool CanAccumulate(int sum, const std::vector<int>& numbers)
 	{
 		int remain = sum - e;
 
-		if (CanAccumulate(remain, numbers) == true)
+		if (CanAccumulate(remain, numbers, h) == true)
 		{
+			h[sum] = true;
 			return true;
 		}
 	}
 
+	h[sum] = false;
 	return false;
 }
 
 int main()
 {
-	std::cout << CanAccumulate(8, { 2, 3, 5 }) << std::endl;
-	std::cout << CanAccumulate(7, { 5, 3, 4, 7 }) << std::endl;
-	std::cout << CanAccumulate(720, { 10, 50, 100, 500 }) << std::endl;
-	std::cout << CanAccumulate(1000, { 7, 14 }) << std::endl;
+	history h;
+	
+	std::cout << CanAccumulate(8, { 2, 3, 5 }, h) << std::endl;
+	h.clear();
+
+	std::cout << CanAccumulate(7, { 5, 3, 4, 7 }, h) << std::endl;
+	h.clear();
+
+	std::cout << CanAccumulate(720, { 10, 50, 100, 500 }, h) << std::endl;
+	h.clear();
+
+	std::cout << CanAccumulate(1000, { 7, 14 }, h) << std::endl;
+	h.clear();
 }
